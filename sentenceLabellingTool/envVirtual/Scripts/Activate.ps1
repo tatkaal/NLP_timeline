164bb9e3c36fba1000d2a0eac59a002ac0b818d5
tatkaal <zeradt@gmail.com>
2020-07-02 12:33:17 +0545

    testing git lfs for adding virtual env to repo

--- sentenceLabellingTool/envVirtual/Scripts/Activate.ps1
+++ sentenceLabellingTool/envVirtual/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\Users\zerad\Desktop\dolphinlab-master\datalabeller\envVirtual"
+$env:VIRTUAL_ENV="C:\Users\zerad\Desktop\git_repo\NLP_timeline\sentenceLabellingTool\envVirtual"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
