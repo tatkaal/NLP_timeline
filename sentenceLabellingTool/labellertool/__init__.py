8af7143f40b9d952b183e56c9d781ce96e2883c5
tatkaal <zeradt@gmail.com>
2020-07-03 11:25:03 +0545

    added CORS access for browser

--- sentenceLabellingTool/labellertool/__init__.py
+++ sentenceLabellingTool/labellertool/__init__.py
@@ -2,8 +2,15 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+import logging
+from logging.handlers import RotatingFileHandler
+from flask_cors import CORS
 
 app = Flask(__name__)
+
+#this forces browser to access the CORS policy
+CORS(app)
+
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
@@ -11,5 +18,9 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+
+handler = RotatingFileHandler('foo.log', maxBytes=10000, backupCount=1)
+handler.setLevel(logging.ERROR)
+app.logger.addHandler(handler)
 
 from labellertool import routes
