2bfa446430c98dafb26104f45c3eefda4fba6bd9
slimbu <sagar.limbu@infodevelopers.com.np>
2020-07-07 04:55:07 -0400

    testing

--- sentenceLabellingTool/nltk_downloader.py
+++ sentenceLabellingTool/nltk_downloader.py
@@ -1,5 +1,3 @@
 import nltk
-nltk.download('all')
-nltk.download()
 nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
--- sentenceLabellingTool/run.py
+++ sentenceLabellingTool/run.py
@@ -4,5 +4,5 @@ from labellertool import app
 if __name__ == "__main__":
     # run the app
     # application.run(debug=True)
-    # app.run(host = '0.0.0.0', port = 8005,debug=True)
-    app.run(host = '127.0.0.1', port = 8005,debug=True)
+    app.run(host = '0.0.0.0', port = 8005,debug=True)
+    #app.run(host = '127.0.0.1', port = 8005,debug=True)
