5b597a246513ff89d922113fa0988090a89d9deb
aigeal-eight <zeradt@gmail.com>
2020-07-13 23:20:57 +0545

    compatibility check for linux server

--- sentenceLabellingTool/labellertool/appendTable.py
+++ sentenceLabellingTool/labellertool/appendTable.py
@@ -3,19 +3,19 @@ import chilkat
 from pathlib import Path
 import os
 import logging
-import winreg
+# import winreg
 from labellertool.config import labelledDataPath
 
-def get_download_path():
-    """Returns the default downloads path for linux or windows"""
-    if os.name == 'nt':
-        sub_key = r'SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders'
-        downloads_guid = '{374DE290-123F-4565-9164-39C4925E467B}'
-        with winreg.OpenKey(winreg.HKEY_CURRENT_USER, sub_key) as key:
-            location = winreg.QueryValueEx(key, downloads_guid)[0]
-        return location
-    else:
-        return os.path.join(os.path.expanduser('~'), 'downloads')
+# def get_download_path():
+#     """Returns the default downloads path for linux or windows"""
+#     if os.name == 'nt':
+#         sub_key = r'SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders'
+#         downloads_guid = '{374DE290-123F-4565-9164-39C4925E467B}'
+#         with winreg.OpenKey(winreg.HKEY_CURRENT_USER, sub_key) as key:
+#             location = winreg.QueryValueEx(key, downloads_guid)[0]
+#         return location
+#     else:
+#         return os.path.join(os.path.expanduser('~'), 'downloads')
 
 # path_to_download_folder = str(os.path.join(Path.home(), "Downloads"))
 # path_to_download_folder = get_download_path()
